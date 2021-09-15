cbd4db1a41a5f550ab9ef2b37b52d6130a37a30a
kemal genc <fatihkemalgenc@hotmail.com>
2021-09-15 13:49:51 +0300

    başlangıç

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni deserunt nulla ipsam officiis, quia fuga iste eligendi, soluta alias sed sapiente ab reiciendis eveniet nostrum, unde neque vitae ipsa voluptates.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni deserunt nulla ipsam officiis, quia fuga iste eligendi, soluta alias sed sapiente ab reiciendis eveniet nostrum, unde neque vitae ipsa voluptates.</p>
+    </div> 
+    );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+    return ( 
+        <div>
+            <h1>All Ninja List</h1>
+        </div>
+     );
+}
+ 
+export default Ninjas;
